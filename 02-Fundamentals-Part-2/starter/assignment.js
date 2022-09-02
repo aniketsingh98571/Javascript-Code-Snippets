799c496c8311d1f1d78d926bcf26aa847ba15ff2
aniketsingh98571 <thor98571@gmail.com>
2022-09-02 10:19:22 +0530

    Fundamentals Part 2 completed

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -1,4 +1,4 @@
-//coding challenge
+//coding challenge 1
 console.log("Assignment Js");
 const calcAverage = (score1, score2, score3) => {
   const average = (score1 + score2 + score3) / 3;
@@ -16,3 +16,60 @@ const checkWinner = () => {
   }
 };
 checkWinner();
+
+//Coding Challenge 2
+const calcTip = (billsArray) => {
+  const bills = [];
+  let tipValue;
+  for (let i = 0; i < billsArray.length; i++) {
+    if (billsArray[i] >= 50 && billsArray[i] <= 300) {
+      tipValue = billsArray[i] * 0.15;
+      bills.push(tipValue);
+    } else {
+      tipValue = billsArray[i] * 0.2;
+      bills.push(tipValue);
+    }
+  }
+  return bills;
+};
+const billsArray = [125, 555, 44];
+const tipsArray = calcTip(billsArray);
+console.log(tipsArray);
+const total = (tipsArray, billsArray) => {
+  let total = 0;
+  for (let i = 0; i < tipsArray.length; i++) {
+    total = total + tipsArray[i] + billsArray[i];
+  }
+  return total;
+};
+const totalBill = total(tipsArray, billsArray);
+console.log(totalBill);
+
+//Coding Challenge 3
+const markDetails = {
+  firstName: "Mark",
+  lastName: "Miller",
+  weight: 78,
+  height: 1.69,
+  calcMarkBMI: function () {
+    this.markBMI = this.weight / (this.height * this.height);
+    return this.markBMI;
+  },
+};
+const johnDetails = {
+  firstName: "John",
+  lastName: "Smith",
+  weight: 92,
+  height: 1.95,
+  calcJohnBMI: function () {
+    this.johnBMI = this.weight / (this.height * this.height);
+    return this.johnBMI;
+  },
+};
+const markBMI = markDetails.calcMarkBMI();
+const johnBMI = johnDetails.calcJohnBMI();
+if (markBMI > johnBMI) {
+  console.log(`Mark BMI(${markBMI}) is greater than john BMI (${johnBMI})`);
+} else {
+  console.log(`John BMI(${johnBMI}) is greater than Mark BMI (${markBMI})`);
+}
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -118,4 +118,174 @@ console.log(birthYear.indexOf(2007));
 
 //ES6 feature, it checks whether an element is present in the array or not.
 //it returns true if the element is present else returns false if the element is not present
+//includes uses strict equality operator which means 23==="23" returns false,it doesn't do type coercion
 console.log(birthYear.includes(207));
+
+//Javascript is case-sensitive, which means peter==="PETER" returns false, since they both are way different
+console.log("peter"==="PETER")
+
+const newArray=["Aniket",22,[[5,6],7]]
+console.log(newArray[2][0][1])
+console.log(newArray[2][1])
+
+//Objects
+
+//this syntax is called object literal syntax
+const mySelf={
+  firstName:"Aniket",
+  lastName:"Singh",
+  age:20,
+  job:"SDE",
+  bestFriend:"Somesh"
+}
+console.log(mySelf)
+
+//accessing elements of Objects
+//dot notation/dot operator
+console.log(mySelf.lastName)
+
+//bracket notation
+console.log(mySelf["lastName"])
+
+//Expressions for bracket notation
+const nameKey="Name"
+console.log(mySelf["first"+nameKey])
+console.log(mySelf["last"+nameKey])
+
+// const userInfo=prompt("What do you want to know about me,firstName or lastName or job")
+// console.log(mySelf[userInfo])
+
+//adding new properties to objects
+//dot notation
+mySelf.linkedin="aniketsingh98571"
+
+
+//bracket notation
+mySelf["github"]="aniketsingh98571"
+console.log(mySelf)
+const friends=["Somesh","Pushkar","Gitesh"]
+console.log(`${mySelf.firstName} has ${friends.length} friends and his best friend is ${mySelf.bestFriend}`)
+
+const mySelf2={
+  firstName:"Aniket",
+  lastName:"Singh",
+   job:"SDE",
+  birthYear:1999,
+  bestFriend:"Somesh",
+  friends:["Pushkar","Somesh","Gitesh"],
+  hasDriversLicense:false,
+  calcAge:function(){  //A method
+    console.log(this) //since we are writing the method in the same object so this has reference to the object properties and hance we can 
+    return 2027-this.birthYear;//access the propertios of object in the same object.
+  }
+}
+console.log(mySelf2.calcAge())
+console.log(mySelf2["calcAge"]())       // Accessing function with brackets notation
+
+//storing the calcAge value to the same object
+const mySelf3={
+  firstName:"Aniket",
+  lastName:"Singh",
+  job:"SDE",
+  birthYear:1999,
+  bestFriend:"Somesh",
+  friends:["Pushkar","Somesh","Gitesh"],
+  hasDriversLicense:false,
+  calcAge:function(){  //A method
+      this.newAge=2027-this.birthYear;  //creating a new variable "newAge" inside same object with "this" keyword since "this" has reference
+      return this.newAge                             //to "mySelf3" object.
+  }
+}
+console.log(mySelf3.calcAge())
+console.log(mySelf3["calcAge"]())
+console.log(mySelf3.newAge);
+
+const mySelf4={
+  firstName:"Aniket",
+  lastName:"Singh",
+  job:"SDE",
+  birthYear:1999,
+  bestFriend:"Somesh",
+  friends:["Pushkar","Somesh","Gitesh"],
+  hasDriversLicense:false,
+  checkDriverLicense:function(){
+    if(this.hasDriversLicense)
+      return "a"
+    else
+      return "no"
+  },
+  calcAge:function(){  //A method
+    this.newAge=2027-this.birthYear;  
+    return this.newAge                            
+  },
+  mySummary:function(){  //A method
+      return `${this.firstName} is a ${this.calcAge()} old ${this.job}, and he has ${this.checkDriverLicense()} drivers
+      licence  `
+  }
+}
+console.log(mySelf4.mySummary())
+
+//for loops
+for(let i=1;i<=10;i++){
+  console.log(` repetition ${i}`)
+}
+
+//using for loop to iterate and array
+const customArray=["Daniel","Richer","Costaricka",55,42,true]
+for(let i=0;i<customArray.length;i++){
+  console.log(customArray[i])
+}
+const newArray1=[];
+for(let i=0;i<customArray.length;i++){
+  const type=typeof(customArray[i])
+  newArray1.push(type)
+}
+console.log(newArray1)
+
+//continue keyword ---->it skips a particular iteration based on specified condition
+const newArray2=["Aniket","Chris","Hola","Cola"]
+for(let i=0;i<newArray2.length;i++){
+  if(newArray2[i]==="Chris")
+    continue;
+  else  
+    console.log(newArray2[i])
+}
+
+//break keyword - it breaks the loop and come out of loop if a condition is met.
+const newArray3=["Aniket","Chris","Hola","Cola"]
+for(let i=0;i<newArray3.length;i++){
+  if(newArray3[i]==="Chris")
+    break;
+  else  
+    console.log(newArray3[i])
+}
+
+//backward looping
+const newArray4=["Aniket","Chris","Hola","Cola"]
+for(let i=newArray4.length-1;i>=0;i--){
+  console.log(newArray4[i])
+}
+
+//Nested Loops
+let counter=0;
+for(let i=0;i<3;i++){
+  for(let j=0;j<5;j++){
+    counter++;
+    console.log("Repetition Number "+counter)
+  }
+}
+
+//while loop
+let i=1;
+while(i<=10){
+  console.log("Repetion Number "+i)
+  i++;
+}
+
+let dice=Math.trunc(Math.random()*6)+1
+console.log(dice)
+while(dice!==6){
+  console.log("You rolled "+dice)
+  dice=Math.trunc(Math.random()*6)+1
+}
+
