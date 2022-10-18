4ec6d99447bf8120b8ec0881a270398c8472f417
aniketsingh98571 <thor98571@gmail.com>
2022-10-18 15:57:04 +0530

    completed strings with challenges

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -267,7 +267,16 @@ const game = {
     ],
   ],
   score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels','Gnarby','Aniket','Hummels','Aniket'],
+  scored: [
+    'Lewandowski',
+    'Gnarby',
+    'Lewandowski',
+    'Hummels',
+    'Gnarby',
+    'Aniket',
+    'Hummels',
+    'Aniket',
+  ],
   date: 'Nov 9th, 2037',
   odds: {
     team1: 1.33,
@@ -389,36 +398,32 @@ for (const [goalNo, playerName] of game.scored.entries()) {
 }
 
 const oddsEntries = Object.entries(game.odds);
-console.log(oddsEntries)
-let average = sum = 0;
-for (const [property, [value,name]] of oddsEntries.entries()) {
- sum=sum+name
+console.log(oddsEntries);
+let average = (sum = 0);
+for (const [property, [value, name]] of oddsEntries.entries()) {
+  sum = sum + name;
 }
-console.log(sum)
-average=sum/oddsEntries.length
-console.log(average)
-const printOddsEntries=Object.entries(game.odds)
-for(const [index,[property,value]] of printOddsEntries.entries()){
-  console.log(`Odd of victory ${game[property]||'draw'}: ${value}`)
+console.log(sum);
+average = sum / oddsEntries.length;
+console.log(average);
+const printOddsEntries = Object.entries(game.odds);
+for (const [index, [property, value]] of printOddsEntries.entries()) {
+  console.log(`Odd of victory ${game[property] || 'draw'}: ${value}`);
 }
-const scorers={
-
-}
-for(let i=0;i<game.scored.length;i++){
-let count=1;
-  for(let j=i+1;j<game.scored.length;j++){
-    if(game.scored[i]===game.scored[j]){
+const scorers = {};
+for (let i = 0; i < game.scored.length; i++) {
+  let count = 1;
+  for (let j = i + 1; j < game.scored.length; j++) {
+    if (game.scored[i] === game.scored[j]) {
       count++;
-      game.scored[j]=-1
+      game.scored[j] = -1;
     }
-     
   }
 
-  if(game.scored[i]!==-1)
-    scorers[game.scored[i]]=count
+  if (game.scored[i] !== -1) scorers[game.scored[i]] = count;
 }
 // scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-console.log(scorers)
+console.log(scorers);
 
 //SETS in JS
 //SETS contains only unique elements, SETS are also iterables like arrays and objects
@@ -426,97 +431,364 @@ console.log(scorers)
 //Since JS is case-sensitive 'Pasta' and 'pasta' are different
 //Set is basically an object which takes arrays as input
 //In SET, elements are randomly placed
-const orderedSet=new Set(['Pasta','Pizza','Pumpkin','Pizza','pasta'])
-const orderSet2=new Set([1,2,3,4,1])
-console.log(orderSet2)
-console.log(orderedSet)
+const orderedSet = new Set(['Pasta', 'Pizza', 'Pumpkin', 'Pizza', 'pasta']);
+const orderSet2 = new Set([1, 2, 3, 4, 1]);
+console.log(orderSet2);
+console.log(orderedSet);
 
 //checking size of SET
-console.log(orderedSet.size)
+console.log(orderedSet.size);
 
 //checking if an element is present in SET or not
-console.log(orderedSet.has('Pizza'))
-console.log(orderedSet.has('Bruschetta'))
+console.log(orderedSet.has('Pizza'));
+console.log(orderedSet.has('Bruschetta'));
 
 //adding element in the SET
-orderedSet.add('Bruschetta')
-console.log(orderedSet)
+orderedSet.add('Bruschetta');
+console.log(orderedSet);
 
 //deleting element in SET
-orderedSet.delete("Pizza")
-console.log(orderedSet)
+orderedSet.delete('Pizza');
+console.log(orderedSet);
 
 //looping over the SET
-for(const data of orderedSet)
-  console.log(data)
+for (const data of orderedSet) console.log(data);
 
 //clearing the entire set
-orderedSet.clear()
-console.log(orderedSet)
+orderedSet.clear();
+console.log(orderedSet);
 
 //Usecase for SET- suppose if we want an array with only unique elements then we can use set.
-const staff=['Pankaj','Mohammad','Ismail','Pankaj','Kendra','Ismail']
+const staff = ['Pankaj', 'Mohammad', 'Ismail', 'Pankaj', 'Kendra', 'Ismail'];
 //since we want an array, thats why we have used spread operator here
-const staffSet=[...new Set(staff)]
-console.log(staffSet)
-
+const staffSet = [...new Set(staff)];
+console.log(staffSet);
 
 //Maps in JS
 //In object, the keys are string. We cannot assign any other data-type as keys in objects.
 //In object, if we add same key name multiple times then it will take the last key and its value.
-const obj8={
-  1:"Aniket",
-  Hi:"Singh",
-  Hi:"Hello"
-}
-console.log(obj8)
-console.log(typeof Object.keys(obj8))
+const obj8 = {
+  1: 'Aniket',
+  Hi: 'Singh',
+  Hi: 'Hello',
+};
+console.log(obj8);
+console.log(typeof Object.keys(obj8));
 //As we can see
-for(const data of Object.keys(obj8))
-  console.log(typeof data)
+for (const data of Object.keys(obj8)) console.log(typeof data);
 
-const rest = new Map()
-rest.set('name','Italian')
-rest.set(1,'Italian')
-rest.set(1,'Italian1')
-console.log(rest)
-for(const [data,value] of rest){
-  console.log(typeof data)
+const rest = new Map();
+rest.set('name', 'Italian');
+rest.set(1, 'Italian');
+rest.set(1, 'Italian1');
+console.log(rest);
+for (const [data, value] of rest) {
+  console.log(typeof data);
 }
 
 //getting values from map
-console.log(rest.get('name'))
+console.log(rest.get('name'));
 
 //checking if a key is present or not in map
-console.log(rest.has('name'))
+console.log(rest.has('name'));
 
 //deleting a key with its value from map
-rest.delete(1)
-console.log(rest)
+rest.delete(1);
+console.log(rest);
 
 //checking the size of map
-console.log(rest.size)
+console.log(rest.size);
 
 //clearing the entire map
-rest.clear()
-console.log(rest)
+rest.clear();
+console.log(rest);
 
 //Initializing Map
-const rest2=new Map([
-  [1,"One"],
-  [2,"Two"],
-  [3,"Three"]
-])
-console.log(rest2)
+const rest2 = new Map([
+  [1, 'One'],
+  [2, 'Two'],
+  [3, 'Three'],
+]);
+console.log(rest2);
 
 //Iterating over Maps
-for(const [key,value] of rest2)
-  console.log(value)
+for (const [key, value] of rest2) console.log(value);
 
 //converting a map into an array
-const mapArray=[...rest2]
-console.log(mapArray)
+const mapArray = [...rest2];
+console.log(mapArray);
 
 //Data Structure in JS as of now
 //1. Array  2. Object  3.Set  4. Map
 
+//Coding CHhallenge 3
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+const bufferEvents = [];
+for (const [key, value] of gameEvents) {
+  bufferEvents.push(value);
+}
+console.log(bufferEvents);
+const events = [...new Set(bufferEvents)];
+console.log(events);
+gameEvents.delete(64);
+console.log(gameEvents);
+
+for (const [key, value] of gameEvents) {
+  if (key <= 90) {
+    console.log('An event happened, on average, every 9 minutes', value);
+  }
+}
+for (const [key, value] of gameEvents) {
+  if (key <= 45) {
+    console.log(`[FIRST HALF] ${key}: ${value}`);
+  } else {
+    console.log(`[SECOND HALF] ${key}: ${value}`);
+  }
+}
+
+//Strings
+const airline = 'Tap Air Portugal';
+const plane = 'A321';
+//strings in JS are arrays in which elements can be acccessed by indexes
+console.log(airline[0]);
+console.log(plane[3]);
+console.log('Aniket'[2]);
+
+//getting length of string
+console.log(plane.length);
+console.log(airline.length);
+
+//getting index at which a particular character or word is present.
+//indexOf returns only first occurence of the character or word
+console.log(airline.indexOf('p'));
+console.log(airline.indexOf('Portugal'));
+
+//to get the lastindex of which the element is present, use the below code
+console.log(airline.lastIndexOf('r'));
+
+//slice method is used to extract some part of the string.
+//it makes use of imdexes of string to extract characters
+//the first parameter of slice is the started index of string and the second parameter is ending index of string like till where we have to extract it.
+//slice always ignores the last index value and calculates the string with lastindex-1
+console.log(airline.slice(0, 2));
+
+//indexOf can be used with slice to get your first name and last name -a usecase
+const myName = 'Aniket Singh';
+//to get the firstName
+console.log(myName.slice(0, myName.indexOf(' ')));
+//to get the lastName
+console.log(myName.slice(myName.indexOf(' ') + 1));
+const checkMiddleSeat = seat => {
+  //B and E are middle seat
+  const lastChar = seat.slice(-1);
+  if (lastChar === 'B' || lastChar === 'E') {
+    console.log('It is a middle seat');
+  } else {
+    console.log('It is not middle seat');
+  }
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+//we can use negative indexes with slice to extract characters from end of the string
+console.log(myName.slice(-1));
+
+//Well you may be curious how we can access functions on strings, since strings are primitive types. When we create a string, JS internally converts it into a string object something like below":-
+console.log(typeof new String('Aniket'));
+//After a particular operation is done on the string, the functions returns the string in primitive type
+
+//Also note that strings are immutable in JS since they are primitve types, so if we are using any functions on strings that do some operations it does not affect the original string.
+
+//UPPERCASE and LOWERCASE
+const myName2 = 'Aniket';
+//lets convert it into UPPERCASE
+console.log(myName2.toUpperCase());
+console.log(myName2);
+
+//lets convert it to lowercase
+console.log(myName2.toLowerCase());
+
+//Suppose you receive a input like "aNiKet", and you want to convert it into "Aniket", then follow below steps:-
+const myName3 = 'aNiKet';
+const LowerName = myName3.toLowerCase();
+const FirstUpper = LowerName[0].toUpperCase() + LowerName.slice(1);
+console.log(FirstUpper);
+
+//Suppose you have a string named " aniket" and you want to remove the whitespace from the string.
+const myName4 = ' aniket';
+console.log(myName4.trim());
+
+//Suppose some user inputs his email as " aniKeT@SinGH", but we wanted "aniket@singh", follow below steps to achieve it
+const email = ' aniKeT@SinGH';
+//since when we apply any operation on strings using some functions, that function returnss the new string and then again we can apply some other function to the string, this is called chaining.
+const properEmail = email.toLowerCase().trim();
+console.log(properEmail);
+
+//Suppose you want to replace "#" in an email with '@' becuase that is correct one :D , follow below steps:-
+const myEmail = 'aniket#singh';
+console.log(myEmail.replace('#', '@'));
+
+//replace only replaces first occurrence of letter or word
+const myEmail2 = 'aniket singh is earning 12# but he returns 15#';
+//In this only 12# will convert to 12$ but 15# will not convert, since replace works on first occurence of a letter.
+console.log(myEmail2.replace('#', '$'));
+
+//In order to prevent, single occurrence replacement we use replaceAll
+console.log(myEmail2.replaceAll('#', '$'));
+
+const myName12 = 'Aniket Singh';
+console.log(myName12.replace('Singh', 'Chaudhari'));
+//checking if particular substring is present in string or not
+const name7 = 'Aniket Singh';
+console.log(name7.includes('ket'));
+console.log(name7.includes(''));
+console.log(name7.includes('nik'));
+
+//check if a string starts with a particular substring
+//it takes first word of string into account
+const name8 = 'Aniket kumar Singh';
+console.log(name8.startsWith('Ani'));
+console.log(name8.startsWith('Anik'));
+
+//check if a string ends with a particular substring
+//it takes last word of string into account
+const name9 = 'Aniket Singh';
+console.log(name9.endsWith('Singh'));
+console.log(name9.endsWith('h'));
+console.log(name9.endsWith('kumar'));
+
+const checkBaggage = items => {
+  const newItem = items.toLowerCase();
+  if (newItem.includes('knife') || newItem.includes('gun')) {
+    console.log('You are not allowed');
+  } else {
+    console.log('You are allowed,Welcome');
+  }
+};
+checkBaggage('I have a laptop, some food and a pocket knife');
+checkBaggage('Socks and Camera');
+checkBaggage('Got some snacks and a gun for protectors');
+
+//Split in Strings
+const sentenceTwo = 'A-very-interesting-string';
+console.log(sentenceTwo.split('-'));
+const sentenceOne = 'Aniket Singh';
+console.log(sentenceOne.split('', 2));
+const sentenceThree = 'JS is fun';
+console.log(sentenceThree.split(' ', 1));
+
+//Join in Strings
+const sentenceFour = ['JS', 'is', 'again'];
+console.log(sentenceFour.join(' '));
+console.log(sentenceFour.join(''));
+console.log(sentenceFour.join('-'));
+console.log(sentenceFour.join('You know'));
+const sentenceFive = ['Mr.', 'Aniket', 'Singh'];
+console.log(sentenceFive.join(' '));
+let nameMe = [];
+//Making First Letter of each word capital
+const capitalize = word => {
+  const names = word.split(' ');
+  for (const data of names) {
+    console.log('Single', data);
+    nameMe.push(data[0].toUpperCase() + data.slice(1));
+  }
+  console.log(nameMe.join(' '));
+};
+capitalize('aniket singh');
+
+//padding in strings
+//padding is useful in masking the credit card number on websites
+const name71 = 'Aniket';
+console.log(name71.padStart(4, '*'));
+
+//coding challenge 4
+const camelCaseList = [];
+const convertToUnderScore = variableList => {
+  for (const name of variableList) {
+    const lowerName = name.toLowerCase();
+    const [firstName, secondName] = lowerName.split('_');
+    const updatedString =
+      firstName + secondName[0].toUpperCase() + secondName.slice(1);
+    camelCaseList.push(updatedString);
+  }
+  console.log(camelCaseList);
+};
+convertToUnderScore([
+  'underscore_case',
+  'first_name',
+  'Some_Variable',
+  'calculate_AGE',
+  'delayed_departure',
+]);
+//underscoreCase firstName someVariable calculateAge delayedDeparture
+
+//coding challenge 4.1
+const flightFormat = schedule => {
+  const removePlus = schedule.split('+');
+  const removedUnderscore = [];
+  const journeyTrack = [];
+  console.log(removePlus);
+  for (const data of removePlus) {
+    removedUnderscore.push(
+      data.replaceAll('_', ' ').replaceAll(':', 'h').trim().split(';')
+    );
+  }
+  console.log(removedUnderscore);
+  for (const data of removedUnderscore) {
+    const tempJourney = {};
+    tempJourney.from = data[1].slice(0, 3);
+    tempJourney.to = data[2].slice(0, 3);
+    journeyTrack.push(tempJourney);
+  }
+  console.log(journeyTrack);
+
+  //final print
+  for (const [index, data] of removedUnderscore.entries()) {
+    console.log(
+      `${data[0]} from ${journeyTrack[
+        index
+      ].from.toUpperCase()} to ${journeyTrack[index].to.toUpperCase()} (${
+        data[3]
+      }) `
+    );
+  }
+};
+
+const flightsCheck =
+  '_Delayed_Departure;fao937410;txl2133456;11:25 +_Arrival;bru0512352;fao93745;11:45 +_Delayed_Arrival;hel452652;fao9854123;12:05 +_Departure;fao85412;lis232252;12:30';
+
+flightFormat(flightsCheck);
+//Delayed Departure from FAO to TXL (11h25)
+// const removePlus = schedule[0].replace('_', '').replaceAll(':', 'h').split('+');
+//   console.log(removePlus);
+//   let removeColon = [];
+//   const formatedPlaces = [];
+//   for (const data of removePlus) {
+//     removeColon.push(data.split(';'));
+//   }
+//   console.log(removeColon);
+//   for (const data of removeColon) {
+//     let formatedPlace = {};
+//     formatedPlace.from = data[1].slice(0, 3);
+//     formatedPlace.to = data[2].slice(0, 3);
+//     formatedPlaces.push(formatedPlace);
+//   }
+//   console.log(formatedPlaces);
+//   for (const data of removeColon) {
+//     // let firstWord = console.log(firstWord);
+//   }
